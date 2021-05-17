410a08b7de07f2f8d4eb5232ee717639d3f87749
rmccownISoft <60158596+rmccownISoft@users.noreply.github.com>
2021-05-16 23:09:12 -0500

    Update client.js
    Add Dayton's code for dynamic text fields.  Updated getrows so it builds url for get request

--- client/client.js
+++ client/client.js
@@ -5,123 +5,83 @@ new Ractive({
 	target: 'body',
 	template: require('./client.html'),
 	data: {
-		// msg: {
-		// 	signupdate: null,
-		// 	vendor_factor: 99,
-		// 	system_factor: 0.99,
-		// },
+
 		aggregatorRow: '',
 		replikwandoRow: '',
 		differenceRow: '',
-		//columnValue: [],
+		productCode: '',
+		fields: [],
 		htpTables: [
-
-			// {
-			// 	table: "companyinfo",
-			// 	columns: {
-			// 		"store": '',
-			// 		"productcode": '',
-			// 	},
-			// },
 			{
 				table: "companyinfo",
-				columns: [ "productcode", "store" ],
+				columns: [ "store" ],
 			},
 			{
 				table: "inventory",
-				columns: [ "productcode", "store", "partnum" ],
+				columns: [ "store", "partnum" ],
 			},
 			{
 				table: "invmaster",
-				columns: [ "productcode", "stocknum", "store" ],
+				columns: [ "stocknum", "store" ],
 			},
 			{
 				table: "mnfcrmod",
-				columns: [ "productcode", "typenum", "pmodel", "htpmnfcrmodid" ],
+				columns: [ "typenum", "pmodel" ],
 			},
 			{
 				table: "model",
-				columns: [ "productcode", "Model", "htpmodelid" ],
+				columns: [ "Model" ],
 			},
 			{
 				table: "partuse",
-				columns: [ "productcode", "typenum" ],
+				columns: [ "typenum" ],
 			},
 			{
 				table: "partlistauthoritymap",
-				columns: [ "productcode", "sourceid" ],
+				columns: [ "sourceid" ],
 			},
 		],
 	},
 	components: {
 		itButton: makeITButton(),
 	},
-	computed: {
-		tableColumns() {
-			const selectedTable = this.get('selectedTable')
-			const tables = this.get('htpTables')
 
-			if (selectedTable) {
-				const matchingTable = tables.find(table => table.table === selectedTable.table)
-				//return (matchingTable && matchingTable.columns) || []
-				return (matchingTable.columns) || []
-			}
-			return []
-		},
-	},
 	oninit() {
 		const ractive = this
-		ractive.observe('selectedTable', table => {
-			console.log('selected table: ', table)
-			ractive.getColumns(table)
-		})
 
-		// Loads a test row
-		//ractive.loadReplikwandoRow()
-		ractive.observe('tableColumns', tableColumns => {
-			//the table columns have changed,
-			//come up with default values for the columns
-			const fieldValues = tableColumns.reduce((sum, column) => {
-				return { ...sum, [column]: '' }
-			}, {})
-			console.log("field values: ", fieldValues)
-			ractive.set({ fieldValues })
+		ractive.observe('selectedTable', selectedTable => {
+			let fields = []
+			if (selectedTable) {
+				const matchingTable = ractive.get('htpTables')
+					.find(table => table.table === selectedTable.table)
+				if (matchingTable) {
+					fields = matchingTable.columns.map(col => {
+						return { label: col, value: '' }
+					})
+				}
+			}
+			ractive.set({ fields })
 		})
 	},
-	getColumns(table) {
-		console.log("columns: ", table.columns)
-		return table.columns
-	},
-	getRows() {
-		// const test = this.get('columnValue')
-		// get the table and find the matching columns
-		// try to run a get on the value for each of the columns
-		// log the result
-		// run a fetch
-		const stuff = this.get('tableColumns')
-	},
-	doFetchAndSet(endPoint, attributeName) {
-		return doFetch(endPoint).then(res=> {
+
+	doFetchAndSet(endPoint, attributes) {
+		console.log("fetch and set attributes: ", attributes)
+		return doFetch(endPoint, attributes).then(res=> {
+			console.log('response: ', res)
 			if (res) {
-				console.log("do fetch and set res: ", res)
 				this.set({ replikwandoRow: res.replikwando })
 				this.set({ aggregatorRow: res.aggregator })
 				this.set({ differenceRow: res.difference })
 			}
 		})
 	},
-	doFetchAndSetComparison(endPoint) {
-		return doFetchComparison(endPoint).then(res=> {
-			if (res) {
-				console.log(res)
-			}
-		})
-	},
-	loadReplikwandoRow() {
-		this.doFetchAndSet('get-companyinfo', 'replikwandoRow')
-	},
-	loadComparisonRows() {
-		this.doFetchAndSetComparison('get-companyinfo')
+	getRows(productCode, tableInfo, fields) {
+		const attributes = createReqObj(fields)
+		const params = new URLSearchParams(attributes).toString()
+
+		const url = `compare-rows/${productCode}/${tableInfo.table}?${params}`
+		this.doFetchAndSet(url, attributes)
+		// Fetch the rows
 	},
 })
 
@@ -133,9 +93,10 @@ async function doFetch(endPoint, data = {}) {
 	return json
 }
 
-async function doFetchComparison(endPoint, data = {}) {
-	const res = await fetch(endPoint, data)
-	const json = await res.json()
-	return json
+function createReqObj(fields) {
+	let reqObj = {}
+	fields.forEach(row => {
+		reqObj[row.label] = row.value
+	})
+	return reqObj
 }
-
