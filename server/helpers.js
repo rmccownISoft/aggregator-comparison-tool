c06ce7f5cb89de7f775f23a4704230e5c7a08569
rmccownISoft <60158596+rmccownISoft@users.noreply.github.com>
2021-05-16 23:07:22 -0500

    Update helpers.js
    Needed a crappy add where clause function

--- server/helpers.js
+++ server/helpers.js
@@ -12,6 +12,22 @@ function appendToQuery(q, sql, values) {
 	return q
 }
 
+function addWhere(baseQuery, queryParameters) {
+	let query = baseQuery
+	// const values
+	if (queryParameters.length === 0) {
+		return query
+	}
+	for (const [ key, value ] of Object.entries(queryParameters)) {
+		//values.push(key, value)
+		//query = query.concat(" AND ?? = ?")
+		query = query.concat(` AND ${key} = ${value}`)
+	}
+	//return [query, values]
+	return query
+}
+
 module.exports = {
 	appendToQuery,
+	addWhere,
 }
